c6fe46bd6405d5964a7e143514a8c81989947199
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-25 12:24:23 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Arrays_and_List_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter name");
+            var userName = Console.ReadLine();
+            Console.WriteLine(userName);    
+        }
+    }
+}
